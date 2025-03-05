ccafa1a4b8b482e1c8c730a19c5b304fd0678748
almaz <tyj5resd@gmail.com>
2025-03-05 04:04:29 +0300

    init

---
+++ src/main/java/com/aaalace/hsecoinbase/HseCoinbaseApplication.java
@@ -0,0 +1,13 @@
+package com.aaalace.hsecoinbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HseCoinbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HseCoinbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aaalace/hsecoinbase/HseCoinbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaalace.hsecoinbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HseCoinbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
